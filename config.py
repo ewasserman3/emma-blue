5317f9ffb3b72e8e5a0190d384d34f6ed9921393
Eric Wasserman <ewasserman89@gmail.com>
2017-10-16 19:35:56 -0400

    Modify config.py

--- config.py
+++ config.py
@@ -1,7 +1,28 @@
 import os
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
-threads = int(os.environ.get('GUNICORN_THREADS', '1'))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-forwarded_allow_ips = '*'
-secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
