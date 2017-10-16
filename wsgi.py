a170bcf6c217be9061d47dda73c6d02c57046246
Eric Wasserman <ewasserman89@gmail.com>
2017-10-15 21:11:46 -0400

    Change app to application

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import datetime
  
  
-app = Flask(__name__)
+application = Flask(__name__)
 
 
 tabs = [
@@ -29,16 +29,16 @@ tabs = [
 ]
 
 
-@app.route("/main")
+@application.route("/main")
 def main_site():
     return render_template('index.html', tabs=tabs)
 
 
-@app.route("/")
+@application.route("/")
 def landing_page():
     return render_template('landing-page.html', tabs=tabs)
 
 
 if __name__ == "__main__":
-    app.run()
-#app.run(host="0.0.0.0", debug=True, port=8080)
+    application.run()
+#application.run(host="0.0.0.0", debug=True, port=8080)
