7b1ba8f0731a70fb9c57ee9d4c1d0ded4bed2651
Eric Wasserman <30412278+ewasserman3@users.noreply.github.com>
2017-10-16 12:40:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def landing_page():
 
 
 if __name__ == "__main__":
-    application.run()
-#application.run(host="0.0.0.0", debug=True, port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    application.run(host='0.0.0.0', port=port, debug=True, use_reloader=True))
